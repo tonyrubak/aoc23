24dff10ec1d89c49a9652be16297ebb9a22c8122
Joe Peralta <joe@crimsonhexagon.us>
2023-12-04 10:10:00 -0500

    day 4 complete

--- lib/day03.ex
+++ lib/day03.ex
@@ -1,4 +1,34 @@
 defmodule Aoc23.Day03 do
+@doc """
+You and the Elf eventually reach a gondola lift station; he says the gondola lift will take you up to the water source, but this is as far as he can bring you. You go inside.
+
+It doesn't take long to find the gondolas, but there seems to be a problem: they're not moving.
+
+"Aaah!"
+
+You turn around to see a slightly-greasy Elf with a wrench and a look of surprise. "Sorry, I wasn't expecting anyone! The gondola lift isn't working right now; it'll still be a while before I can fix it." You offer to help.
+
+The engineer explains that an engine part seems to be missing from the engine, but nobody can figure out which one. If you can add up all the part numbers in the engine schematic, it should be easy to work out which part is missing.
+
+The engine schematic (your puzzle input) consists of a visual representation of the engine. There are lots of numbers and symbols you don't really understand, but apparently any number adjacent to a symbol, even diagonally, is a "part number" and should be included in your sum. (Periods (.) do not count as a symbol.)
+
+Here is an example engine schematic:
+
+467..114..
+...*......
+..35..633.
+......#...
+617*......
+.....+.58.
+..592.....
+......755.
+...$.*....
+.664.598..
+
+In this schematic, two numbers are not part numbers because they are not adjacent to a symbol: 114 (top right) and 58 (middle right). Every other number is adjacent to a symbol and so is a part number; their sum is 4361.
+
+Of course, the actual engine schematic is much larger. What is the sum of all of the part numbers in the engine schematic?
+"""
   def read_data do
     {:ok, content} = File.read("data/day3.txt")
     content
@@ -36,16 +66,15 @@ defmodule Aoc23.Day03 do
       |> generate_lines
     poss_gears = find_gears(map)
 
-    n =
-      poss_gears
-      |> Enum.map(fn {:symbol, %{x: x, y: y}} -> neighbors(map, x, y) end)
-      |> Enum.map(fn ns -> Enum.map(ns, fn n -> map_lookup(map, n) end) end)
-      |> Enum.map(fn ns -> Enum.filter(ns, &is_number?/1) end)
-      |> Enum.map(&MapSet.new/1)
-      |> Enum.filter(fn ns -> MapSet.size(ns) == 2 end)
-      |> Enum.map(&MapSet.to_list/1)
-      |> Enum.map(fn [{:number, %{val: l}}, {:number, %{val: r}}] -> l * r end)
-      |> Enum.sum
+    poss_gears
+    |> Enum.map(fn {:symbol, %{x: x, y: y}} -> neighbors(map, x, y) end)
+    |> Enum.map(fn ns -> Enum.map(ns, fn n -> map_lookup(map, n) end) end)
+    |> Enum.map(fn ns -> Enum.filter(ns, &is_number?/1) end)
+    |> Enum.map(&MapSet.new/1)
+    |> Enum.filter(fn ns -> MapSet.size(ns) == 2 end)
+    |> Enum.map(&MapSet.to_list/1)
+    |> Enum.map(fn [{:number, %{val: l}}, {:number, %{val: r}}] -> l * r end)
+    |> Enum.sum
   end
 
   def is_number?({:number, _}), do: true
---
+++ lib/day04.ex
@@ -0,0 +1,112 @@
+defmodule Aoc23.Day04 do
+  def read_data do
+    {:ok, content} = File.read("data/day4.txt")
+    content
+    |> String.split("\n")
+    |> Enum.reverse
+    |> tl
+    |> Enum.reverse
+  end
+
+  def process_data(data) do
+    data
+    |> Enum.map(&Aoc23.Day04Parser.parse/1)
+    |> Enum.map(&score_card/1)
+    |> Enum.sum
+  end
+
+  def score_card({:ok, [_, {:winning, winning}, {:ours, ours}], _, _, _, _}) do
+    MapSet.intersection(MapSet.new(winning), MapSet.new(ours))
+    |> MapSet.size
+    |> then(fn count -> count - 1 end)
+    |> then(fn adj_count -> if adj_count >= 0 do 2 ** adj_count else 0 end end)
+  end
+
+  def process_data2(data) do
+    cards =
+      data
+      |> Enum.map(&Aoc23.Day04Parser.parse/1)
+
+    num_cards = length(cards)
+
+    cards_won_cards = for {card, n} <- Enum.zip(cards, 1..num_cards), reduce: [] do
+      acc -> [{n, score_card2(card, n)} | acc]
+    end
+    
+    card_winnings =
+      cards_won_cards
+      |> Enum.map(fn card -> only_existing_cards(card, num_cards) end)
+      |> Enum.into(%{})
+
+    k = Map.keys(card_winnings)
+    Enum.flat_map(k, fn key -> Enum.filter(Map.get(card_winnings, key), fn it -> it == key end) end)
+
+    reduce_cards(Enum.to_list(1..num_cards), card_winnings)
+    |> Map.values
+    |> Enum.sum
+  end
+
+  def reduce_cards(cards, winning), do: reduce_cards(cards, winning, %{})
+  def reduce_cards([], _, result), do: result
+  def reduce_cards([card | rest], winning, result) do
+    {result, copies} = case Map.get(result, card) do
+      nil -> {Map.put(result, card, 1), 1}
+      n -> {result, n}
+    end
+
+    won = case Map.get(winning, card) do
+      nil -> []
+      list -> list
+    end
+
+    new_result = for won_card <- won, reduce: result do
+      acc -> Map.update(acc, won_card, 1 + copies, fn val -> val + copies end)
+    end
+    reduce_cards(rest, winning, new_result)
+  end
+
+  def only_existing_cards({card, winnings}, max_card) do
+    winnings = Enum.filter(winnings, fn n -> n <= max_card end)
+    {card, winnings}
+  end
+
+  def score_card2({:ok, [_, {:winning, winning}, {:ours, ours}], _, _, _, _}, i) do
+    MapSet.intersection(MapSet.new(winning), MapSet.new(ours))
+    |> MapSet.size
+    |> then(fn n -> if n > 0 do Enum.to_list((i+1)..(i+n)) else [] end end)
+  end
+end
+
+
+
+
+defmodule Aoc23.Day04Parser do
+  import NimbleParsec
+
+  separator =
+    ignore(string("|"))
+    |> replace(:sep)
+
+  whitespace = repeat(string(" "))
+
+  winning_numbers = 
+    repeat(concat(integer(min: 1), ignore(whitespace)))
+    |> tag(:winning)
+
+  our_numbers = 
+    repeat(concat(integer(min: 1), ignore(whitespace)))
+    |> tag(:ours)
+
+  card =
+    ignore(string("Card"))
+    |> concat(ignore(whitespace))
+    |> concat(unwrap_and_tag(integer(min: 1), :id))
+    |> concat(ignore(string(":")))
+    |> concat(ignore(whitespace))
+    |> concat(winning_numbers)
+    |> concat(ignore(separator))
+    |> concat(ignore(whitespace))
+    |> concat(our_numbers)
+  
+  defparsec(:parse, card)
+end
